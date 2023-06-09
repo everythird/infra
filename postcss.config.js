51806718b91af314ab865732fb06712b95c0d344
Ray Elder <hello@rayelder.com>
2023-06-08 21:39:32 -0700

    Project clean up

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('postcss-preset-env'),
-        require('cssnano'),
-    ],
+  plugins: [
+    require('postcss-import'),
+    require('postcss-preset-env')({ stage: 1 }),
+    // require('cssnano'),
+  ],
 };
